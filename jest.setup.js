67251ed7d7044535ade1833e3b21218d6d6c12f4
njoki <njerinjoki22@gmail.com>
2026-07-09 09:11:17 +0300

    fix test env config & broken pokemon page test

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
-/* global require, global */
 import '@testing-library/jest-dom';
-const { TextEncoder, TextDecoder } = require('util');
+import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
