a586c5a02a5f4773eccc4585387bab3665d9435c
njoki <njerinjoki22@gmail.com>
2026-07-09 08:46:32 +0300

    fix linting errors

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,6 @@
-const { TextEncoder, TextDecoder } = require('util')
+/* global require, global */
+import '@testing-library/jest-dom';
+const { TextEncoder, TextDecoder } = require('util');
 
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
