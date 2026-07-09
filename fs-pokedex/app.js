508a806e0bfa63baa4b8a015a46ba366eac5f5b3
njoki <njerinjoki22@gmail.com>
2026-07-10 00:01:20 +0300

    fix nested git tracking and upload actual pokedex files

---
+++ fs-pokedex/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+// get the port from env variable
+const PORT = process.env.PORT || 5001
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error... ')
+  res.send('ok')
+})
+
+app.use(express.static('dist'))
+
+const start = async () => {
+  await app.listen(PORT)
+  console.log(`server started on port ${PORT}`)
+}
+
+start()
---
+++ fs-pokedex/jest.setup.js
@@ -0,0 +1,6 @@
+/* global global */
+import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
